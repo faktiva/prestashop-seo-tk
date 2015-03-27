1559ce98990a9c56438ed9f3f78b55a583b377a1
Antony Maker <amaker@faktiva.com>
2015-03-27 14:45:00 +0100

    First import

---
+++ zzseotk.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * 
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0)
+ * It is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ * @author	 ZiZuu.com <info@zizuu.com>
+ * @license	http://opensource.org/licenses/afl-3.0.php	Academic Free License (AFL 3.0)
+ * @source	 https://github.com/ZiZuu-store/PrestaShop_module-CleanURLs
+ */
+
+if (!defined('_PS_VERSION_'))
+	exit;
+	
+class zzSEOtk extends Module
+{
+	public function __construct()
+	{
+		$this->name = 'zzseotk';
+		$this->tab = 'seo';
+		$this->version = '0.1';
+		$this->author = 'ZiZuu Store';
+		$this->need_instance = 0;
+		$this->ps_versions_compliancy = array('min' => '1.5', 'max' => _PS_VERSION_);
+		$this->bootstrap = true;
+	 
+		parent::__construct();
+	 
+		$this->displayName = $this->l('ZiZuu SEO ToolKit');
+		$this->description = $this->l('Handles a few basic SEO related improvements such as "hreflang" and "canonical".');
+	 
+		$this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
+	}
+		
+	public function install()
+	{
+		if (Shop::isFeatureActive())
+			Shop::setContext(Shop::CONTEXT_ALL);
+
+		return (parent::install()
+			&& $this->registerHook('header')
+		);
+	}
+	
+	public function uninstall()
+	{
+		return parent::uninstall();
+	}		
+	
+	public function hookDisplayHeader()
+	{
+		$this->context->smarty->assign(
+			array(
+				'link' => $this->context->link->getModuleLink($this->name, 'display')
+			)
+		);
+		return $this->display(__FILE__, 'hreflang.tpl');
+	}
+}
+
