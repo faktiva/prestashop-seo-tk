4accaf26ff9834db4c69989aefa9c4d6802a1480
drAlberT <albert@faktiva.com>
2015-07-17 18:19:58 +0200

    Added auto-update scripts

---
+++ upgrade/install-0.1.1.php
@@ -0,0 +1,11 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+ 
+function upgrade_module_0_1_1($module)
+{
+    Configuration::deleteByName('ZZSEOTK_SEO_PAGINATION_FACTOR');
+    return true;
+}
---
+++ upgrade/install-0.1.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+ 
+function upgrade_module_0_1($module)
+{
+    return true;
+}
