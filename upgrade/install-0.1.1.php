e57eda309d28dca4f974b8b7e154a890752f0f7e
ZiZuu-store <info@ZiZuu.com>
2015-07-23 10:38:51 +0200

    added upgrade script

--- upgrade/install-0.1.1.php
+++ upgrade/install-0.1.1.php
@@ -3,9 +3,10 @@
 if (!defined('_PS_VERSION_')) {
     exit;
 }
- 
+
 function upgrade_module_0_1_1($module)
 {
     Configuration::deleteByName('ZZSEOTK_SEO_PAGINATION_FACTOR');
+
     return true;
 }
