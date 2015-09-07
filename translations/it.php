fd7f7ecd4363df16f7e1513d4db852fb8d874ec2
Emiliano 'AlberT' Gabrielli <albert@faktiva.com>
2015-09-07 15:34:39 +0200

    Added italian translation

---
+++ translations/it.php
@@ -0,0 +1,17 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{zzseotk}prestashop>zzseotk_d1b5d1ea0dbe349716008f89d715ff5f'] = 'ZiZuu SEO ToolKit';
+$_MODULE['<{zzseotk}prestashop>zzseotk_315d8274c8fb8f894beb852533177009'] = 'Gestisce alcune migliorie per il SEO, come i meta tag \"canonical\", \"hreflang\" e \"noindex\"';
+$_MODULE['<{zzseotk}prestashop>zzseotk_2721730cf9ab3db75b92b8d0556750a5'] = 'Sicuro di voler rimuovere il modulo \"ZiZuu SEO ToolKit\" ? ';
+$_MODULE['<{zzseotk}prestashop>zzseotk_70b4903abcb62ace84264ad0443ae759'] = 'Internazionalizzazione';
+$_MODULE['<{zzseotk}prestashop>zzseotk_17492e133c9b260db5fc9b3bf2d5869c'] = 'Abilita il meta tag \"hreflang\"';
+$_MODULE['<{zzseotk}prestashop>zzseotk_0dc76fc867387bdc9b8f5bbfbb35b60f'] = 'Inserisce il meta tag \"hreflang\" nell\'head HTML per gestire l\'indicizzazione di contenuti nelle diverse lingue';
+$_MODULE['<{zzseotk}prestashop>zzseotk_c9cc8cce247e49bae79f15173ce97354'] = 'Salva';
+$_MODULE['<{zzseotk}prestashop>zzseotk_ee096b45f2e0dcd83771ea27b049c76a'] = 'URL canonica';
+$_MODULE['<{zzseotk}prestashop>zzseotk_2639c7053e5ce27740118bc8172d584c'] = 'Abilita il meta tag \"canonical\"';
+$_MODULE['<{zzseotk}prestashop>zzseotk_df8ee74025f9cb35538aeb6ea2b524de'] = 'Inserisce il meta tag \"canonical\" nell\'head HTML per evitare problemi di contenuti duplicati nel SEO.';
+$_MODULE['<{zzseotk}prestashop>zzseotk_a8066a7d2d58ab65978cf66c9e852d92'] = 'indicizzazione';
+$_MODULE['<{zzseotk}prestashop>zzseotk_2c0c9d8693247eddceb40c9e78b7666d'] = 'Abilita il meta tag \"noindex\"';
+$_MODULE['<{zzseotk}prestashop>zzseotk_93ea8146f3aaf364151f5b92f694c517'] = 'Inserisce il meta tag \"noindex\" nell\'head HTML impedendo l\'indicizzazione delle pagine \"private\" da parte dei motori di ricerca.';
