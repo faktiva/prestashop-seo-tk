e55b44a9c344e3271ca686e2b678474a31e75e32
ZiZuu-store <info@ZiZuu.com>
2016-02-10 12:10:35 +0100

    Release 1.1.7
    - added composer / debugging capabilities
    - add noindex when selected_filters is present

--- zzseotk.php
+++ zzseotk.php
@@ -80,7 +80,7 @@ class zzseotk extends Module
         $this->name = 'zzseotk';
         $this->author = 'ZiZuu Store';
         $this->tab = 'seo';
-        $this->version = '1.1.5';
+        $this->version = '1.1.7';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = array('min' => '1.5.0.1', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
