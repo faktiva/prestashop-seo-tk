02a48a2c3c61e1cba44289bbcc04a60c4e4812c8
drAlberT <albert@faktiva.com>
2016-04-08 16:58:15 +0200

    Release 1.2.2

--- zzseotk.php
+++ zzseotk.php
@@ -80,10 +80,10 @@ class zzseotk extends Module
         $this->name = 'zzseotk';
         $this->author = 'ZiZuu Store';
         $this->tab = 'seo';
-        $this->version = '1.2.1';
+        $this->version = '1.2.2';
         $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.5.0.1', 'max' => _PS_VERSION_);
         $this->bootstrap = true;
+        $this->ps_versions_compliancy = array('min' => '1.5.0.1', 'max' => _PS_VERSION_);
 
         parent::__construct();
 
