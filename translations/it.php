9063c9cddf925b3b8f45d4277f9ffc4688e1c958
drAlberT <albert@faktiva.com>
2016-10-05 10:17:49 +0200

    Release 1.4.0
    - migration to "faktiva" organization maintainance
    - cleanup PS from "old" module name
    - fixed source files' header

--- translations/it.php
+++ translations/it.php
@@ -1,17 +1,35 @@
 <?php
 
+/*
+ * This file is part of the "Prestashop SEO ToolKit" module.
+ *
+ * (c) Faktiva (http://faktiva.com)
+ *
+ * NOTICE OF LICENSE
+ * This source file is subject to the CC-BY-4.0 license that is
+ * available at the URL https://creativecommons.org/licenses/by/4.0/
+ *
+ * DISCLAIMER
+ * This code is provided as is without any warranty.
+ * No promise of being safe or secure
+ *
+ * @author   AlberT <albert@faktiva.com>
+ * @license  https://creativecommons.org/licenses/by/4.0/  CC-BY-4.0
+ * @source   https://github.com/faktiva/prestashop-seo-tk
+ */
+
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{zzseotk}prestashop>zzseotk_d1b5d1ea0dbe349716008f89d715ff5f'] = 'ZiZuu SEO ToolKit';
-$_MODULE['<{zzseotk}prestashop>zzseotk_315d8274c8fb8f894beb852533177009'] = 'Gestisce alcune migliorie per il SEO, come i meta tag \"canonical\", \"hreflang\" e \"noindex\"';
-$_MODULE['<{zzseotk}prestashop>zzseotk_2721730cf9ab3db75b92b8d0556750a5'] = 'Sicuro di voler rimuovere il modulo \"ZiZuu SEO ToolKit\" ? ';
-$_MODULE['<{zzseotk}prestashop>zzseotk_70b4903abcb62ace84264ad0443ae759'] = 'Internazionalizzazione';
-$_MODULE['<{zzseotk}prestashop>zzseotk_17492e133c9b260db5fc9b3bf2d5869c'] = 'Abilita il meta tag \"hreflang\"';
-$_MODULE['<{zzseotk}prestashop>zzseotk_0dc76fc867387bdc9b8f5bbfbb35b60f'] = 'Inserisce il meta tag \"hreflang\" nell\'head HTML per gestire l\'indicizzazione di contenuti nelle diverse lingue';
-$_MODULE['<{zzseotk}prestashop>zzseotk_c9cc8cce247e49bae79f15173ce97354'] = 'Salva';
-$_MODULE['<{zzseotk}prestashop>zzseotk_ee096b45f2e0dcd83771ea27b049c76a'] = 'URL canonica';
-$_MODULE['<{zzseotk}prestashop>zzseotk_2639c7053e5ce27740118bc8172d584c'] = 'Abilita il meta tag \"canonical\"';
-$_MODULE['<{zzseotk}prestashop>zzseotk_df8ee74025f9cb35538aeb6ea2b524de'] = 'Inserisce il meta tag \"canonical\" nell\'head HTML per evitare problemi di contenuti duplicati nel SEO.';
-$_MODULE['<{zzseotk}prestashop>zzseotk_a8066a7d2d58ab65978cf66c9e852d92'] = 'indicizzazione';
-$_MODULE['<{zzseotk}prestashop>zzseotk_2c0c9d8693247eddceb40c9e78b7666d'] = 'Abilita il meta tag \"noindex\"';
-$_MODULE['<{zzseotk}prestashop>zzseotk_93ea8146f3aaf364151f5b92f694c517'] = 'Inserisce il meta tag \"noindex\" nell\'head HTML impedendo l\'indicizzazione delle pagine \"private\" da parte dei motori di ricerca.';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_d1b5d1ea0dbe349716008f89d715ff5f'] = 'ZiZuu SEO ToolKit';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_315d8274c8fb8f894beb852533177009'] = 'Gestisce alcune migliorie per il SEO, come i meta tag \"canonical\", \"hreflang\" e \"noindex\"';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_2721730cf9ab3db75b92b8d0556750a5'] = 'Sicuro di voler rimuovere il modulo \"ZiZuu SEO ToolKit\" ? ';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_70b4903abcb62ace84264ad0443ae759'] = 'Internazionalizzazione';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_17492e133c9b260db5fc9b3bf2d5869c'] = 'Abilita il meta tag \"hreflang\"';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_0dc76fc867387bdc9b8f5bbfbb35b60f'] = 'Inserisce il meta tag \"hreflang\" nell\'head HTML per gestire l\'indicizzazione di contenuti nelle diverse lingue';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_c9cc8cce247e49bae79f15173ce97354'] = 'Salva';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_ee096b45f2e0dcd83771ea27b049c76a'] = 'URL canonica';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_2639c7053e5ce27740118bc8172d584c'] = 'Abilita il meta tag \"canonical\"';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_df8ee74025f9cb35538aeb6ea2b524de'] = 'Inserisce il meta tag \"canonical\" nell\'head HTML per evitare problemi di contenuti duplicati nel SEO.';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_a8066a7d2d58ab65978cf66c9e852d92'] = 'indicizzazione';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_2c0c9d8693247eddceb40c9e78b7666d'] = 'Abilita il meta tag \"noindex\"';
+$_MODULE['<{faktiva_seo_tk}prestashop>faktiva_seo_tk_93ea8146f3aaf364151f5b92f694c517'] = 'Inserisce il meta tag \"noindex\" nell\'head HTML impedendo l\'indicizzazione delle pagine \"private\" da parte dei motori di ricerca.';
